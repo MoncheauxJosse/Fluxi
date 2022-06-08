f63dc8272d02457ea9bde0ac923ab7fc4054b6db
unknown <lordssly@gmail.com>
2022-06-08 09:17:04 +0200

    ajout css pour le futur caroucel

--- src/corps/corp.js
+++ src/corps/corp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import img from './imageCaroucel/trilogie_noob.jpg'
 import img1 from './imageCaroucel/titanic.jpg'
+import img2 from './imageCaroucel/joker.jpg'
 import  './corp.css'
 
 
@@ -11,15 +12,17 @@ const Corps = () =>{
 
     return(
 
-        
+        <div class="TitreCaroucel">Nouveaux
         <div class="caroucel">
+            <div class="left"></div>
             <div class="item"><img  src={img} alt="img1"/></div>
             <div class="item"><img  src={img1} alt="img2"/></div>
-            <div class="item">3</div>
+            <div class="item"><img  src={img2} alt="img3"/></div>
             <div class="item">4</div>
-            <div class="item">5</div>
-          
+            <div class="item">5</div> 
+            <div class="right"></div>
            
+        </div>
         </div>
 
         
