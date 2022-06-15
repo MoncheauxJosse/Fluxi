d1d2233866b63cdb0d1627d515f638bde162fb4c
unknown <lordssly@gmail.com>
2022-06-15 11:15:03 +0200

    ajout d'un nouveau component et hooks nomé Data

--- src/corps/corp.js
+++ src/corps/corp.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import img from './imageCaroucel/trilogie_noob.jpg'
-import img1 from './imageCaroucel/titanic.jpg'
-import img2 from './imageCaroucel/joker.jpg'
+import React, { useState } from 'react'
+import {Data} from './dataSlide'
 import  './corp.css'
 
 
 const Corps = () =>{
 
-    
+    // import la fonction Data et l ajoute dans un state nomé dataSLide
+    const [dataSlide] = useState(Data);
     
 
+    console.log(dataSlide.length);
     return(
 
         <div class="TitreCaroucel">Nouveaux
         <div class="caroucel">
             <div class="left"></div>
-            <div class="item"><img  src={img} alt="img1"/></div>
-            <div class="item"><img  src={img1} alt="img2"/></div>
-            <div class="item"><img  src={img2} alt="img3"/></div>
+            <div class="item"><img  src={dataSlide[0]} alt="img1"/></div>
+            <div class="item"><img  src={dataSlide[1]} alt="img2"/></div>
+            <div class="item"><img  src={dataSlide[2]} alt="img3"/></div>
             <div class="item">4</div>
             <div class="item">5</div> 
             <div class="right"></div>
