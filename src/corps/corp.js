5dbaff97d29c363b0066e001835f4b21015717a2
unknown <lordssly@gmail.com>
2022-06-17 11:48:14 +0200

    ajout du system de slide (premiére partie)

--- src/corps/corp.js
+++ src/corps/corp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import {Data} from './dataSlide'
 import  './corp.css'
 
@@ -6,27 +6,74 @@ import  './corp.css'
 const Corps = () =>{
 
     // import la fonction Data et l ajoute dans un state nomé dataSLide
-    const [dataSlide] = useState(Data);
+    const [dataSlide, setData] = useState(Data); 
+    const [slideIndex, setSlideIndex] = useState(5)
+    const [afficheFin, setAfficheFin] = useState(5)
+    const [afficheDebut, setAfficheDebut] = useState(0)
+
+
     
 
-    console.log(dataSlide.length);
-    return(
+    const nextSlide = () => {
 
-        <div class="TitreCaroucel">Nouveaux
-        <div class="caroucel">
-            <div class="left"></div>
-            <div class="item"><img  src={dataSlide[0]} alt="img1"/></div>
-            <div class="item"><img  src={dataSlide[1]} alt="img2"/></div>
-            <div class="item"><img  src={dataSlide[2]} alt="img3"/></div>
-            <div class="item">4</div>
-            <div class="item">5</div> 
-            <div class="right"></div>
+        console.log("sa passe")
+        console.log("le slide index "+slideIndex)
+        if(slideIndex !== dataSlide.length){
+
+            console.log("longuer tableaux "+dataSlide.length)
+            setData(dataSlide)
+            setSlideIndex(slideIndex + 1)
+            setAfficheFin(afficheFin +1)
+            setAfficheDebut(afficheDebut +1)
+
+        } 
+        else if (slideIndex === dataSlide.length){
+
+            console.log("slide a la fin")
+            setData(dataSlide)
+            setSlideIndex(5)
+            setAfficheFin(5)
+            setAfficheDebut(0)
+            
+        }
+    }
+
+    const prevSlide = () => {
            
-        </div>
-        </div>
-
+        console.log("le slide index "+slideIndex)
         
+        if(slideIndex !== 0){
+            setData(dataSlide)
+            setSlideIndex(slideIndex - 1)
+            setAfficheFin(afficheFin -1)
+            setAfficheDebut(afficheDebut -1)
+        }
+        else if (slideIndex === 1){
+            setData(dataSlide)
+            setSlideIndex(dataSlide.length)
+        }
+    }
 
+
+    // renvoie une map de dataSlide , obj et index , a chaque fois que la map sort une image il augmente la varibale index .
+    // on appel dans <img le tableaux dataSlide[] avec la variable index a l'interieur , ainsi on appel dataslide[0] puis 1 , puis 2 et ect....
+    return (
+        <div className="caroucel">
+            <div class="left" onClick={prevSlide}> {"<"} </div>
+            {dataSlide.map((obj, index) => {
+
+                if( index<afficheFin && index>=afficheDebut ){ 
+                    
+                    return (
+                    <div class="item">
+                        <img src={dataSlide[index]} alt={"img"+(index)}/>
+                    </div>
+                             )
+                    }       
+               
+            })}
+            <div class="right" onClick={nextSlide} >{">"}</div>
+        </div>
     )
 
 
--- src/corps/dataSlide.js
+++ src/corps/dataSlide.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+
 import item from './imageCaroucel/trilogie_noob.jpg'
 import item2 from './imageCaroucel/titanic.jpg'
 import item3 from './imageCaroucel/joker.jpg'
+import item4 from './imageCaroucel/DocteurStrangeMultiverse.jpg'
+import item5 from './imageCaroucel/Kaamelott.jpg'
+import item6 from './imageCaroucel/mortal-affiche-francaise-1348783.jpg'
+import item7 from './imageCaroucel/1917.jpg'
+import item8 from './imageCaroucel/TopGunMaverick.jpg'
 // la fonction Hook (useState) +fonction est mieux qu'une class ceci permet de creer des state ( changment ) dans une fonction
 
 
@@ -11,9 +16,17 @@ export const Data = () =>{
 //Ensuite le const prend 2 valeur , l ettat actuel (data) et une methode qui affichera le changement d'etat ( SetData)
 // ainsi si nous voulons modifier la valeur de data , il faudrat appeler le setData
 
- const [data, setData] = useState([item ,
+ const data =[item ,
     item2,
-    item3])
+    item3,
+    item4,
+    item5,
+   item6,
+   item7,
+   item8]
 
+
+
+    
      return data
 }
