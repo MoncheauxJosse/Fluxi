3dc2fe1ddfabd4c520f1f25c9ed02f40badf75b1
unknown <lordssly@gmail.com>
2022-07-07 20:26:44 +0200

    preparation update backgroud+ methode qui ressort l'id de l image qu'on survole2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import './App.css';
 //import { BrowserRouter } from 'react-router-dom'
 import Navigation from './Nav/index.js';
 import Corps from './corps/corp.js';
+import background from "./imageIndex/BackgroundFluxi.png";
 
 function App() {
-  return (
+
+  const containerStyle = {
+    backgroundImage: `url(${background})`,
+      backgroundPosition: 'center',
+      with:'100%',
+      height: '800px',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+
+      
+    }
+
     
-    <div className="App">
-     <Navigation/>
-     <Corps/>
-    </div>
-  );
+
+    return (
+      <div style={containerStyle}>
+       <Navigation/>
+        <Corps/>
+      </div>
+    );
 }
 
 export default App;
+
